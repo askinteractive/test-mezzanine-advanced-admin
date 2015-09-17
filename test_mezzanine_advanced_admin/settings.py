d3f9b6774461561b2b5f408735349114bf738a61
Aymeric Bois <abois@askin.fr>
2015-09-17 13:21:42 +0200

    Add filebrowser into INSTALLED_APPS

--- test_mezzanine_advanced_admin/settings.py
+++ test_mezzanine_advanced_admin/settings.py
@@ -204,6 +204,7 @@ TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, "templates"),)
 ################
 
 INSTALLED_APPS = (
+    "filebrowser",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
